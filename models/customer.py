16669ff3da551ae1e9fcb74d52abb6d37d2d8edd
alex <alex@2DAM-011>
2025-12-09 11:38:18 +0100

    My first commit on dev_feature2

--- models/customer.py
+++ models/customer.py
@@ -7,11 +7,17 @@ class Customer(models.Model):
     _name = 'g4_bank.customer'
     _description = 'Customer'
 
-    name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
+    first_name = fields.Char()
+    last_name = fields.Char()
+    middle_initial = fields.Char()
+    street = fields.Char()
+    city = fields.Char()
+    state = fields.Char()
+    zip = fields.Integer()
+    phone = fields.Integer()
+    email  = fields.Char()
+    password = fields.Char()
+    
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record in self:
