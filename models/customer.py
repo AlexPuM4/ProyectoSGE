c6f76cef3ae3854acfe629cd9a0820042c5ed72a
alex <alex@2DAM-011>
2025-12-16 14:00:55 +0100

    My first commit on dev_feature2

--- models/customer.py
+++ models/customer.py
@@ -8,16 +8,6 @@ class Customer(models.Model):
     _inherit = 'res.users'
     _description = 'Customer'
 
-    first_name = fields.Char(required = True)
-    last_name = fields.Char(required = True)
-    middle_initial = fields.Char()
-    street = fields.Char(required = True)
-    city = fields.Char(required = True)
-    state = fields.Char(required = True)
-    zip_cp = fields.Integer(required = True)
-    phone = fields.Integer(required = True)
-    email  = fields.Char(required = True)
-    password = fields.Char(required = True)
     account_ids = fields.Many2Many('g4_bank.account')
     
 #     @api.depends('value')
