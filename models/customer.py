837f9bcdc8bfb79fe1f1f53b5b19646cef81accc
alex <alex@2DAM-011>
2026-01-16 10:59:59 +0100

    Trabajo actual de la rama dev_Account

--- models/customer.py
+++ models/customer.py
@@ -7,7 +7,16 @@ class Customer(models.Model):
     _name = 'g4_bank.customer'
     _inherit = 'res.users'
     _description = 'Customer'
-
+    
+    firstName = fields.Text()
+    lastName = fields.Text()
+    middleInitial = fields.Text()
+    street = fields.Text()
+    city = fields.Text()
+    state = fields.Text()
+    email = fields.Text()
+    password = fields.Text()
+    
     account_ids = fields.Many2Many('g4_bank.account')
     
 #     @api.depends('value')
