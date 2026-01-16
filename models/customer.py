fd314460954b0016e50eb00e42206eaba6ee3ec2
alex <alex@2DAM-011>
2026-01-16 12:20:11 +0100

    Trabajo actual de la rama dev_Account

--- models/customer.py
+++ models/customer.py
@@ -4,18 +4,8 @@ from odoo import models, fields, api
 
 
 class Customer(models.Model):
-    _name = 'g4_bank.customer'
     _inherit = 'res.users'
     _description = 'Customer'
-    
-    firstName = fields.Text()
-    lastName = fields.Text()
-    middleInitial = fields.Text()
-    street = fields.Text()
-    city = fields.Text()
-    state = fields.Text()
-    email = fields.Text()
-    password = fields.Text()
     
     account_ids = fields.Many2many('g4_bank.account')
     
