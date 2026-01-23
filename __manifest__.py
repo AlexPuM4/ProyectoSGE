dbb44c69ead14a52734d93e3945e4554a6bc1e9e
alex <alex@2DAM-011>
2026-01-23 11:54:06 +0100

    Trabajo actual de la rama dev_Account

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/Bank.xml',
         'views/Customer.xml',
         'views/Account.xml',
--- models/account.py
+++ models/account.py
@@ -13,8 +13,8 @@ class Account(models.Model):
     beginBalance = fields.Float(string ="Begin Balance",required = True)
     beginBalanceTimestamp = fields.Date(string="Begin Balance Timestamp",required = True)
     typeAccount = fields.Selection([('STANDART', 'Standart'),('CREDIT','Credit')],required = True)
-    customer_ids = fields.Many2many('g4_bank.customer')
-    #movement_ids= fields.One2Many('g4_bank.movement','account_id')
+    customer_ids = fields.Many2many('res.users')
+    #movement_ids= fields.One2many('g4_bank.movement','account_id')
     
 
 #     @api.depends('value')
